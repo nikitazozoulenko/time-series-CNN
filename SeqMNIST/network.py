868addcbccf94a4549ead55a838495cf9c9a7449
nikitazozoulenko <nikita.zozoulenko@gmail.com>
2018-04-03 13:42:29 +0200

    tidied upp network implementation code

---
+++ SeqMNIST/network.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+
+#inspiration: https://arxiv.org/pdf/1803.01271.pdf
+
+class ResidualBlock1D(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, kernel_size, dilation, dropout_chance = 0.2):
+        super(ResidualBlock1D, self).__init__()
+        depth = 2
+        self.layers = nn.ModuleList()
+        for i in range(depth):
+            c_in = hidden_size if i != 0 else input_size
+            c_out = hidden_size if i != depth-1 else output_size
+            pad = torch.nn.ZeroPad2d(((kernel_size-1)*dilation, 0))
+            conv = nn.utils.weight_norm(nn.Conv1d(c_in, c_out, kernel_size, stride=1, dilation=dilation))
+            relu = nn.ReLU()
+            dropout = nn.Dropout(dropout_chance)
+            self.layers.append(nn.Sequential(pad, conv, relu, dropout))
+
+        self.channel_scaling = None
+        if input_size != output_size:
+            self.channel_scaling = nn.utils.weight_norm(nn.Conv1d(input_size, output_size, kernel_size=1, stride=1))
+
+
+    def forward(self, x):
+        res = x
+        x = self.layers[0](x)
+        x = self.layers[1](x)
+        if self.channel_scaling != None:
+            res = self.channel_scaling(x)
+        return res + x
+
+
+class TCNSingle(nn.Module):
+    def __init__(self, n_layers, input_size, hidden_size, output_size, kernel_size, dilation_lambda = lambda l: 1+l):
+        super(TCNSingle, self).__init__()
+        blocks = []
+        for l in range(n_layers):
+            dil = dilation_lambda(l)
+            c_in = hidden_size if i != 0 else input_size
+            c_out = hidden_size if i != n_layers-1 else output_size
+            blocks += [ResidualBlock1D(c_in, hidden_size, c_out, kernel_size, dil, dropout_chance = 0.2)]
+        self.residual_blocks = nn.Sequential(*blocks)
+
+
+    def forward(self, x):
+        out = self.residual_blocks(x)
+        return out
+
+
+class RadicalResidualBlock(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, kernel_size, dilation, dropout_chance = 0.2):
+        super(RadicalResidualBlock, self).__init__()
+        depth = 4
+        self.layers = nn.ModuleList()
+        for i in range(depth):
+            c_in = hidden_size if i != 0 else input_size
+            c_out = hidden_size if i != depth-1 else output_size
+            pad = torch.nn.ZeroPad2d(((kernel_size-1)*dilation, 0))
+            conv = nn.utils.weight_norm(nn.Conv1d(c_in, c_out, kernel_size, stride=1, dilation=dilation))
+            relu = nn.ReLU()
+            dropout = nn.Dropout(dropout_chance)
+            self.layers.append(nn.Sequential(pad, conv, relu, dropout))
+
+        self.scaling_in = None
+        if input_size != hidden_size:
+            self.scaling_in = nn.utils.weight_norm(nn.Conv1d(input_size, hidden_size, kernel_size=1, stride=1))
+        self.scaling_out = None
+        if output_size != hidden_size:
+            self.scaling_out = nn.utils.weight_norm(nn.Conv1d(hidden_size, output_size, kernel_size=1, stride=1))
+        self.scaling_large = None
+        if output_size != input_size:
+            self.scaling_large = nn.utils.weight_norm(nn.Conv1d(input_size, output_size, kernel_size=1, stride=1))
+
+
+    def forward(self, x):
+        res0 = x
+        res1 = self.layers[0](res0)
+        res2 = self.layers[1](res1)
+        if self.scaling_in != None:
+            res2 += self.scaling_in(res0)
+        else:
+            res2 += res0
+        res3 = self.layers[2](res2)
+        res3 += res1
+        res4 = self.layers[3](res3)
+        if self.scaling_out != None:
+            res4 += self.scaling_out(res2)
+        else:
+            res4 += res2
+        if self.scaling_large != None:
+            res4 += self.scaling_large(res0)
+        else:
+            res4 += res0
+        return res4
+
+
+class TCNRadical(nn.Module):
+    def __init__(self, n_layers, input_size, hidden_size, output_size, kernel_size, dilation_lambda = lambda l: 1+l):
+        super(TCNRadical, self).__init__()
+        blocks = []
+        for i in range(n_layers):
+            dil = dilation_lambda(i)
+            c_in = hidden_size if i != 0 else input_size
+            c_out = hidden_size if i != n_layers-1 else output_size
+            blocks += [RadicalResidualBlock(c_in, hidden_size, c_out, kernel_size, dil, dropout_chance = 0.2)]
+
+        self.residual_blocks = nn.Sequential(*blocks)
+
+
+    def forward(self, x):
+        out = self.residual_blocks(x)
+        return out
+
+
+class DoubleResidualBlock(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, kernel_size, dilation, dropout_chance = 0.2):
+        super(DoubleResidualBlock, self).__init__()
+        depth = 2
+        self.layers = nn.ModuleList()
+        for i in range(depth):
+            c_in = hidden_size if i != 0 else input_size
+            c_out = hidden_size if i != depth-1 else output_size
+            pad = torch.nn.ZeroPad2d(((kernel_size-1)*dilation, 0))
+            conv = nn.utils.weight_norm(nn.Conv1d(c_in, c_out, kernel_size, stride=1, dilation=dilation))
+            relu = nn.ReLU()
+            dropout = nn.Dropout(dropout_chance)
+            self.layers.append(nn.Sequential(pad, conv, relu, dropout))
+
+        self.scaling = None
+        if input_size != output_size:
+            self.scaling = nn.utils.weight_norm(nn.Conv1d(input_size, output_size, kernel_size=1, stride=1))
+
+
+    def forward(self, middle_x, last_x):
+        res0 = last_x
+        res1 = self.layers[0](last_x)
+        if middle_x is not None:
+            res1 += middle_x
+        res2 = self.layers[1](res1)
+        if self.scaling != None:
+            res2 += self.scaling(res0)
+        else:
+            res2 += res0
+        return res1, res2
+
+
+class TCNDouble(nn.Module):
+    def __init__(self, n_layers, input_size, hidden_size, output_size, kernel_size, dilation_lambda = lambda l: 1+l):
+        super(TCNDouble, self).__init__()
+        self.n_layers = n_layers
+        self.blocks = nn.ModuleList()
+        for i in range(n_layers):
+            dil = dilation_lambda(i)
+            c_in = hidden_size if i != 0 else input_size
+            c_out = hidden_size if i != n_layers-1 else output_size
+            self.blocks.append(DoubleResidualBlock(c_in, hidden_size, c_out, kernel_size, dil, dropout_chance = 0.2))
+
+
+    def forward(self, x):
+        middle_x = None
+        last_x = x
+        for i in range(self.n_layers):
+            middle_x, last_x = self.blocks[i](middle_x, last_x)
+        return last_x
+
+
+if __name__ == "__main__":
+    import numpy as np
+    from torch.autograd import Variable
+    x = np.arange(1*2*3).reshape(1,2,3)
+    tensor = Variable(torch.zeros(100,1,28*28))
+    print(tensor)
+    model = TCNRadical(n_layers=6, input_size=1, hidden_size=32, output_size=10, kernel_size=3, 
+                        dilation_lambda = lambda l: 1+l*3)
+    print(model(tensor))
---
+++ SeqMNIST/train.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import numpy as np
+
+from network import TCNSingle, TCNDouble, TCNRadical
+from data_feeder import DataFeeder
+from logger import Logger
+
+import random
+
+def create_train_val_test_split(batch_size):
+    mnist_train = datasets.MNIST("/hdd/Data/MNIST/", train=True, transform=transforms.ToTensor(), download=True)
+    mnist_test = datasets.MNIST("/hdd/Data/MNIST/", train=False, transform=transforms.ToTensor(), download=True)
+
+    indices = random.sample(range(60000), 5000)
+    train_data = []
+    val_data = []
+    test_data = []
+    for i, ex in enumerate(mnist_train):
+        if i in indices:
+            val_data += [ex]
+        else:
+            train_data += [ex]
+    for ex in mnist_test:
+        test_data += [ex]
+
+    return train_data, val_data, test_data
+
+        
+def make_batch(batch_size, i, data, use_cuda = True, volatile = True):
+    images = []
+    labels = []
+    for j in range(batch_size):
+        image, label = data[(i+j) % len(data)]
+        images += [image]
+        labels += [label]
+    images_tensor = torch.stack(images).resize_(batch_size, 1, 28*28)
+    labels_tensor = torch.LongTensor(labels)
+    if use_cuda:
+        images_tensor = images_tensor.cuda()
+        labels_tensor = labels_tensor.cuda()
+    return Variable(images_tensor, volatile=volatile), Variable(labels_tensor, volatile=volatile)
+
+
+def evaluate_acc(batch_size, model, data, i, val_loss, val_acc, permute):
+    model.eval()
+
+    images, labels = data
+    accs = []
+    losses = []
+    for j in range(0, len(images), batch_size):
+        #loss
+        batch_images = images[j:j+batch_size]
+        batch_labels = labels[j:j+batch_size]
+
+        if permute is not None:
+            batch_images = batch_images[:,:,permute]
+        
+        pred = model(batch_images)[:,:,-1]
+        losses += [F.cross_entropy(pred, batch_labels)]
+
+        #acc
+        _, index = pred.topk(1, dim=1)
+        accs += [torch.mean((index.squeeze() == batch_labels).float())]
+        
+    acc = torch.mean(torch.stack(accs))
+    print(acc)
+    loss = torch.mean(torch.stack(losses))
+    val_loss.write_log(loss, i)
+    val_acc.write_log(acc, i)
+
+    model.train()
+    
+        
+def train(model, optimizer, images, labels, i, train_logger, permute):
+    optimizer.zero_grad()
+    if permute is not None:
+        images = images[:,:,permute]
+    # only take the loss of the last time step
+    pred = model(images)[:,:,-1]
+    loss = F.cross_entropy(pred, labels)
+    loss.backward()
+    optimizer.step()
+
+    train_logger.write_log(loss, i)
+
+
+def increase_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        print("updated learning rate: new lr:", param_group['lr']*10)
+        param_group['lr'] = param_group['lr']*10
+
+
+def decrease_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        print("updated learning rate: new lr:", param_group['lr']/10)
+        param_group['lr'] = param_group['lr']/10
+    
+
+def main():
+    batch_size = 100
+    train_data, val_data, test_data = create_train_val_test_split(batch_size)
+    data_feeder = DataFeeder(train_data, preprocess_workers = 1, cuda_workers = 1, cpu_size = 10,
+                 cuda_size = 10, batch_size = batch_size, use_cuda = True, volatile = False)
+    data_feeder.start_queue_threads()
+    val_data = make_batch(len(val_data), 0, val_data, use_cuda = True, volatile = True)
+    test_data = make_batch(len(test_data), 0, test_data, use_cuda = True, volatile = True)
+
+    single = TCNSingle(n_layers=12, input_size=1, hidden_size=32, output_size=10, kernel_size=6, 
+                        dilation_lambda = lambda l: int(1+l*1.4)).cuda()
+    double = TCNDouble(n_layers=12, input_size=1, hidden_size=32, output_size=10, kernel_size=6, 
+                        dilation_lambda = lambda l: int(1+l*1.4)).cuda()
+    radical = TCNRadical(n_layers=6, input_size=1, hidden_size=32, output_size=10, kernel_size=6, 
+                        dilation_lambda = lambda l: 1+l*3).cuda()
+    optimizer_single = optim.SGD(single.parameters(), lr=0.01, momentum=0.9, weight_decay=0.00001)
+    optimizer_double = optim.SGD(double.parameters(), lr=0.01, momentum=0.9, weight_decay=0.00001)
+    optimizer_radical = optim.SGD(radical.parameters(), lr=0.01, momentum=0.9, weight_decay=0.00001)
+
+    single_train_loss = Logger("single_train_losses.txt")
+    single_val_loss = Logger("single_val_losses.txt")
+    single_val_acc = Logger("single_val_acc.txt")
+    double_train_loss = Logger("double_train_losses.txt")
+    double_val_loss = Logger("double_val_losses.txt")
+    double_val_acc = Logger("double_val_acc.txt")
+    radical_train_loss = Logger("radical_train_losses.txt")
+    radical_val_loss = Logger("radical_val_losses.txt")
+    radical_val_acc = Logger("radical_val_acc.txt")
+
+    permute = Variable(torch.from_numpy(np.random.permutation(28*28)).long().cuda(), requires_grad=False)
+    #permute = None
+
+    for i in range(200001):
+        images, labels = data_feeder.get_batch()
+        train(single, optimizer_single, images, labels, i, single_train_loss, permute)
+        train(double, optimizer_double, images, labels, i, double_train_loss, permute)
+        train(radical, optimizer_radical, images, labels, i, radical_train_loss, permute)
+        if i % 100 == 0:
+            print(i)
+            evaluate_acc(batch_size, single, val_data, i, single_val_loss, single_val_acc, permute)
+            evaluate_acc(batch_size, double, val_data, i, double_val_loss, double_val_acc, permute)
+            evaluate_acc(batch_size, radical, val_data, i, radical_val_loss, radical_val_acc, permute)
+        if i in [1000]:
+            increase_lr(optimizer_single)
+            increase_lr(optimizer_double)
+            increase_lr(optimizer_radical)
+        if i in [150000, 180000]:
+            decrease_lr(optimizer_single)
+            decrease_lr(optimizer_double)
+            decrease_lr(optimizer_radical)
+        if i % 1000 == 0:
+            torch.save(single.state_dict(), "savedir/single_it"+str(i//1000)+"k.pth")
+            torch.save(double.state_dict(), "savedir/double_it"+str(i//1000)+"k.pth")
+            torch.save(radical.state_dict(), "savedir/radical_it"+str(i//1000)+"k.pth")
+            
+    data_feeder.kill_queue_threads()
+
+    import evaluate
+    evaluate.main(permute)
+
+    
+if __name__ == "__main__":
+    main()
